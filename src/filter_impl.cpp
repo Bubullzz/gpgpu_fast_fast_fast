87904e86b19cf1be604d01723d10129d4adcecd8
Breeetzel <luca.descrambes@mailbox.org>
2025-06-11 19:21:11 +0200

    feat: added cpu branch

--- src/filter_impl.cpp
+++ src/filter_impl.cpp
@@ -4,26 +4,27 @@
 #include <thread>
 #include "logo.h"
 
-struct rgb {
+struct rgb
+{
     uint8_t r, g, b;
 };
 
-extern "C" {
-    void filter_impl(uint8_t* buffer, int width, int height, int stride, int pixel_stride)
+extern "C"
+{
+    void filter_impl(uint8_t *buffer, int width, int height, int stride, int pixel_stride)
     {
         for (int y = 0; y < height; ++y)
         {
-            rgb* lineptr = (rgb*) (buffer + y * stride);
+            rgb *lineptr = (rgb *)(buffer + y * stride);
             for (int x = 0; x < width; ++x)
             {
-                lineptr[x].r = 0; // Back out red component
 
                 if (x < logo_width && y < logo_height)
                 {
                     float alpha = logo_data[y * logo_width + x] / 255.f;
-                    lineptr[x].g = uint8_t(alpha * lineptr[x].g + (1-alpha) * 255);
-                    lineptr[x].b = uint8_t(alpha * lineptr[x].b + (1-alpha) * 255);
-
+                    lineptr[x].r = uint8_t(alpha * lineptr[x].r + (1 - alpha) * 255);
+                    lineptr[x].g = uint8_t(alpha * lineptr[x].g + (1 - alpha) * 255);
+                    lineptr[x].b = uint8_t(alpha * lineptr[x].b + (1 - alpha) * 255);
                 }
             }
         }
@@ -31,7 +32,7 @@ extern "C" {
         // You can fake a long-time process with sleep
         {
             using namespace std::chrono_literals;
-            //std::this_thread::sleep_for(100ms);
+            // std::this_thread::sleep_for(100ms);
         }
-    }   
+    }
 }
